e9d5481a64f21b8ee109a98c10da4d091f5af420
Halinen <kurisushiina@gmail.com>
2022-02-14 10:22:32 +0800

    update

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AppService } from './app.service';
+
+@Controller()
+@ApiTags("App 总模块")
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  //@ApiOperation()表示一个http请求的操作
+  @ApiOperation({
+    summary: "测试接口"
+  })
+  getHello(): string {
+    return this.appService.getGoodbye();
+  }
+}
---
+++ src/db/db.module.ts
@@ -0,0 +1,25 @@
+import { Global, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schema/user.schema';
+
+const MONGO_MODELS = MongooseModule.forFeature([
+    {
+        name: "USER_MODEL",
+        schema: UserSchema,
+        collection: "user"
+    }
+])
+@Global()
+@Module({
+    imports: [
+        MongooseModule.forRoot('mongodb://localhost:27017/sora',{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+    }),
+        MONGO_MODELS
+    ],
+    exports: [MONGO_MODELS]
+})
+export class DbModule {}
